0e90261d624f1373ad18d5ce1c89d3ff3bdc10ee
Xu Xu <xuxu@xuxu.name>
2014-01-08 16:23:10 -0800

    Initial Commit

---
+++ Downloader/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Downloader
+//
+//  Created by Xu Xu on 1/8/14.
+//  Copyright (c) 2014 Xu Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
