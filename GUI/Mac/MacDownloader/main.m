02fbcc4cc98429c094b04cee75168e3aa7962eb0
Xu Xu <xuxu@xuxu.name>
2014-01-10 15:31:19 -0800

    add empty mac gui project

---
+++ GUI/Mac/MacDownloader/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacDownloader
+//
+//  Created by Xu Xu on 1/10/14.
+//  Copyright (c) 2014 Xu Xu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
